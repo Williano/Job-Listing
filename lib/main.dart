bfa9b1f9808496bd816329d987e7a2a3c51b8d0a
Williano <paawilly17@gmail.com>
2020-05-06 00:56:43 -0500

    Add device preview

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:job_listing/category_clients_screen.dart';
 import 'package:job_listing/category_list_screen.dart';
+#import 'package:device_preview/device_preview.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(JobListinApp());
 
-class MyApp extends StatelessWidget {
+// void main() => runApp(
+//       DevicePreview(
+//         builder: (context) => JobListinApp(),
+//       ),
+//     );
+
+class JobListinApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
