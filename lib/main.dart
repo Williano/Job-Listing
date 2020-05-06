86ad6840b901d2227886854ac581992c5bf6bbd4
Williano <paawilly17@gmail.com>
2020-05-06 13:54:50 -0500

    Update name

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:job_listing/category_clients_screen.dart';
 import 'package:job_listing/category_list_screen.dart';
-#import 'package:device_preview/device_preview.dart';
+//import 'package:device_preview/device_preview.dart';
 
 void main() => runApp(JobListinApp());
 
